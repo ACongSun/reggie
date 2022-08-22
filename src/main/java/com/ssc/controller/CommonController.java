21fd1a9f22731b7065f2fa2d58fe9805ebd277c9
acongsun <2683885184@qq.com>
2022-08-22 19:48:30 +0800

    文件下载

--- src/main/java/com/ssc/controller/CommonController.java
+++ src/main/java/com/ssc/controller/CommonController.java
@@ -3,12 +3,18 @@ package com.ssc.controller;
 import com.ssc.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -52,4 +58,26 @@ public class CommonController {
         return R.success(s);
     }
 
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+
+        try {
+            // 输入流 读取文件内容
+            FileInputStream inputStream = new FileInputStream(new File(basePath + name));
+            // 输出流将文件写回浏览器，展示图片
+            ServletOutputStream outputStream = response.getOutputStream();
+            response.setContentType("image/jpeg");
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = inputStream.read(bytes)) != -1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            outputStream.close();
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
